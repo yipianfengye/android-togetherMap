be99fe0fe336124488cc0bee6ae835b332c52786
liuchao5 <liuchao5@xiaomi.com>
2016-10-18 15:08:13 +0800

    地图页面添加marker数据

--- app/src/main/java/xiaomi/mich/com/android_togethermap/MainActivity.java
+++ app/src/main/java/xiaomi/mich/com/android_togethermap/MainActivity.java
@@ -1,24 +1,34 @@
 package xiaomi.mich.com.android_togethermap;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.amap.api.maps.AMap;
 import com.amap.api.maps.CameraUpdateFactory;
 import com.amap.api.maps.MapView;
 import com.amap.api.maps.TextureMapView;
+import com.amap.api.maps.model.BitmapDescriptorFactory;
 import com.amap.api.maps.model.CameraPosition;
 import com.amap.api.maps.model.LatLng;
+import com.amap.api.maps.model.Marker;
+import com.amap.api.maps.model.MarkerOptions;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener, AMap.OnMapClickListener {
+import java.util.ArrayList;
+import java.util.List;
+
+import xiaomi.mich.com.android_togethermap.model.DotInfo;
+import xiaomi.mich.com.android_togethermap.utils.DisplayUtil;
+
+public class MainActivity extends AppCompatActivity {
     private MapView mapView;
     private AMap aMap;
-    private Button basicmap;
-    private Button rsmap;
-    private Button nightmap;
-    private Boolean firsttouch = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,13 +47,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         if (aMap == null) {
             aMap = mapView.getMap();
         }
-        aMap.setOnMapClickListener(this);
-        basicmap = (Button)findViewById(R.id.basicmap);
-        basicmap.setOnClickListener(this);
-        rsmap = (Button)findViewById(R.id.rsmap);
-        rsmap.setOnClickListener(this);
-        nightmap = (Button)findViewById(R.id.nightmap);
-        nightmap.setOnClickListener(this);
     }
 
     /**
@@ -53,6 +56,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onResume() {
         super.onResume();
         mapView.onResume();
+
+        initMarker(DotInfo.initData());
     }
 
     /**
@@ -82,46 +87,64 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mapView.onDestroy();
     }
 
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.basicmap:
-                aMap.setMapType(AMap.MAP_TYPE_NORMAL);// 矢量地图模式
-                break;
-            case R.id.rsmap:
-                aMap.setMapType(AMap.MAP_TYPE_SATELLITE);// 卫星地图模式
-                break;
-            case R.id.nightmap:
-                aMap.setMapType(AMap.MAP_TYPE_NIGHT);//夜景地图模式
-                break;
+
+    /**
+     * 初始化marker数据
+     */
+    private void initMarker(List<DotInfo> dotList) {
+        if (dotList == null || dotList.size() == 0) {
+            return;
         }
 
+        for (int i = 0; i < dotList.size(); i++) {
+            DotInfo dotInfo = dotList.get(i);
+
+            MarkerOptions options = new MarkerOptions();
+            options.anchor(0.5f, 1.0f);
+            options.position(new LatLng(dotInfo.getDotLat(), dotInfo.getDotLon()));
+
+            setIconToOptions(dotInfo, options);
+
+            Marker marker = aMap.addMarker(options);
+            marker.setObject(dotInfo);
+            marker.setZIndex(14);
+        }
     }
 
-    @Override
-    public void onMapClick(LatLng arg0) {
-        if (firsttouch) {
-            firsttouch = false;
-            aMap.animateCamera(CameraUpdateFactory.newCameraPosition(new CameraPosition(new LatLng(39.92463,116.389139), 18, 0, 0)), 1500, new AMap.CancelableCallback() {
+    /**
+     * 为marker的Options设置icon
+     * @param dotInfo
+     * @param options
+     */
+    private void setIconToOptions(DotInfo dotInfo, MarkerOptions options) {
+        Bitmap mBitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher);
+        View view = createTextIconByType(dotInfo.getCarTotal() + "", dotInfo, mBitmap);
+        options.icon(BitmapDescriptorFactory.fromView(view));
+    }
 
-                @Override
-                public void onFinish() {
-                    aMap.moveCamera(CameraUpdateFactory.changeTilt(60));
-                    try {
-                        Thread.sleep(2000);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                    aMap.animateCamera(CameraUpdateFactory.changeBearing(90), 2000, null);
 
-                }
+    /**
+     * 根据网点的数量以及选中状态返回Marker的样式
+     *
+     * @param count
+     * @param dotInfo
+     * @return
+     */
+    private View createTextIconByType(String count, DotInfo dotInfo, Bitmap mBitmap) {
 
-                @Override
-                public void onCancel() {
-
-                }
-            });
-
+        View view = LayoutInflater.from(this).inflate(R.layout.map_marker_layout, null);
+        ImageView imageView = (ImageView) view.findViewById(R.id.iv_marker);
+        TextView textView = (TextView) view.findViewById(R.id.tv_marker_number);
+        // 当网点车辆数大于1位时，修改显示网点TextView的宽高
+        if (count.length() > 1) {
+            int size = DisplayUtil.dip2px(this, 22);
+            textView.getLayoutParams().width = size;
+            textView.getLayoutParams().height = size;
         }
+        textView.setText(count);
+        textView.setBackgroundResource(R.drawable.marker_num_nonumber_bg);
+        imageView.setImageResource(R.drawable.ic_location_normal);
+
+        return view;
     }
 }
--- app/src/main/java/xiaomi/mich/com/android_togethermap/model/DotInfo.java
+++ app/src/main/java/xiaomi/mich/com/android_togethermap/model/DotInfo.java
@@ -1,5 +1,8 @@
 package xiaomi.mich.com.android_togethermap.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by aaron on 2016/10/17.
  */
@@ -42,5 +45,75 @@ public class DotInfo {
 
     public void setDotLon(double dotLon) {
         this.dotLon = dotLon;
+    }
+
+    /**
+     * 初始化数据
+     * @return
+     */
+    public static List<DotInfo> initData() {
+        List<DotInfo> dotInfoList = new ArrayList<>();
+
+        DotInfo dotInfo1 = new DotInfo();
+        dotInfo1.setDotLat(39.976318);
+        dotInfo1.setDotLon(116.318988);
+        dotInfoList.add(dotInfo1);
+
+        DotInfo dotInfo2 = new DotInfo();
+        dotInfo2.setDotLat(40.045813);
+        dotInfo2.setDotLon(116.304504);
+        dotInfoList.add(dotInfo2);
+
+        DotInfo dotInfo3 = new DotInfo();
+        dotInfo3.setDotLat(39.923347);
+        dotInfo3.setDotLon(116.507537);
+        dotInfoList.add(dotInfo3);
+
+        DotInfo dotInfo4 = new DotInfo();
+        dotInfo4.setDotLat(39.91125);
+        dotInfo4.setDotLon(116.477378);
+        dotInfoList.add(dotInfo4);
+
+        DotInfo dotInfo5 = new DotInfo();
+        dotInfo5.setDotLat(40.041337);
+        dotInfo5.setDotLon(116.312181);
+        dotInfoList.add(dotInfo5);
+
+        DotInfo dotInfo6 = new DotInfo();
+        dotInfo6.setDotLat(39.971879);
+        dotInfo6.setDotLon(116.306446);
+        dotInfoList.add(dotInfo6);
+
+        DotInfo dotInfo7 = new DotInfo();
+        dotInfo7.setDotLat(40.002434);
+        dotInfo7.setDotLon(116.463232);
+        dotInfoList.add(dotInfo7);
+
+        DotInfo dotInfo8 = new DotInfo();
+        dotInfo8.setDotLat(39.980959);
+        dotInfo8.setDotLon(116.331772);
+        dotInfoList.add(dotInfo8);
+
+        DotInfo dotInfo9 = new DotInfo();
+        dotInfo9.setDotLat(39.925659);
+        dotInfo9.setDotLon(116.508567);
+        dotInfoList.add(dotInfo9);
+
+        DotInfo dotInfo10 = new DotInfo();
+        dotInfo10.setDotLat(39.88543);
+        dotInfo10.setDotLon(116.461778);
+        dotInfoList.add(dotInfo10);
+
+        DotInfo dotInfo11 = new DotInfo();
+        dotInfo11.setDotLat(39.98343);
+        dotInfo11.setDotLon(116.311843);
+        dotInfoList.add(dotInfo11);
+
+        DotInfo dotInfo12 = new DotInfo();
+        dotInfo12.setDotLat(40.029849);
+        dotInfo12.setDotLon(116.313302);
+        dotInfoList.add(dotInfo12);
+
+        return dotInfoList;
     }
 }
