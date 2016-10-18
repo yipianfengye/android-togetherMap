eac385ffcd3607d94f680a1ec187b36b5ca7c503
liuchao5 <liuchao5@xiaomi.com>
2016-10-18 09:34:20 +0800

    初始化地图显示，初始化各种配置信息

--- app/src/main/java/xiaomi/mich/com/android_togethermap/MainActivity.java
+++ app/src/main/java/xiaomi/mich/com/android_togethermap/MainActivity.java
@@ -2,12 +2,126 @@ package xiaomi.mich.com.android_togethermap;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import com.amap.api.maps.AMap;
+import com.amap.api.maps.CameraUpdateFactory;
+import com.amap.api.maps.MapView;
+import com.amap.api.maps.TextureMapView;
+import com.amap.api.maps.model.CameraPosition;
+import com.amap.api.maps.model.LatLng;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, AMap.OnMapClickListener {
+    private MapView mapView;
+    private AMap aMap;
+    private Button basicmap;
+    private Button rsmap;
+    private Button nightmap;
+    private Boolean firsttouch = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mapView = (MapView) findViewById(R.id.map);
+        mapView.onCreate(savedInstanceState);// 此方法必须重写
+        init();
+    }
+
+    /**
+     * 初始化AMap对象
+     */
+    private void init() {
+        if (aMap == null) {
+            aMap = mapView.getMap();
+        }
+        aMap.setOnMapClickListener(this);
+        basicmap = (Button)findViewById(R.id.basicmap);
+        basicmap.setOnClickListener(this);
+        rsmap = (Button)findViewById(R.id.rsmap);
+        rsmap.setOnClickListener(this);
+        nightmap = (Button)findViewById(R.id.nightmap);
+        nightmap.setOnClickListener(this);
+    }
+
+    /**
+     * 方法必须重写
+     */
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    /**
+     * 方法必须重写
+     */
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    /**
+     * 方法必须重写
+     */
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }
+
+    /**
+     * 方法必须重写
+     */
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.basicmap:
+                aMap.setMapType(AMap.MAP_TYPE_NORMAL);// 矢量地图模式
+                break;
+            case R.id.rsmap:
+                aMap.setMapType(AMap.MAP_TYPE_SATELLITE);// 卫星地图模式
+                break;
+            case R.id.nightmap:
+                aMap.setMapType(AMap.MAP_TYPE_NIGHT);//夜景地图模式
+                break;
+        }
+
+    }
+
+    @Override
+    public void onMapClick(LatLng arg0) {
+        if (firsttouch) {
+            firsttouch = false;
+            aMap.animateCamera(CameraUpdateFactory.newCameraPosition(new CameraPosition(new LatLng(39.92463,116.389139), 18, 0, 0)), 1500, new AMap.CancelableCallback() {
+
+                @Override
+                public void onFinish() {
+                    aMap.moveCamera(CameraUpdateFactory.changeTilt(60));
+                    try {
+                        Thread.sleep(2000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    aMap.animateCamera(CameraUpdateFactory.changeBearing(90), 2000, null);
+
+                }
+
+                @Override
+                public void onCancel() {
+
+                }
+            });
+
+        }
     }
 }
